22871a1daa3145037653a4f7fa83763973a3065d
mark-todd <markpeter.todd@hotmail.co.uk>
2023-12-11 16:52:33 +0000

    Added setup bug fix

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="pytest-mypy-testing",
+    entry_points={"pytest11": ["pytest_mypy_testing = pytest_mypy_testing"]},
+    classifiers=["Framework :: Pytest"],
+    version="0.1.0",
+    packages=["pytest_mypy_testing"],
+    install_requires=["pytest>=7.0.0"],
+)
