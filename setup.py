cb09ffea05882b65b0d961da4a7bb1dda316172c
mark-todd <markpeter.todd@hotmail.co.uk>
2023-12-12 00:57:07 +0000

    Fixed plugin

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pytest-mypy-testing",
-    entry_points={"pytest11": ["pytest_mypy_testing = pytest_mypy_testing"]},
+    entry_points={"pytest11": ["pytest_mypy_testing = pytest_mypy_testing.plugin"]},
     classifiers=["Framework :: Pytest"],
     version="0.1.0",
     packages=["pytest_mypy_testing"],
